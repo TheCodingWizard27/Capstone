8b149d4d080f96d8fe914921d92de4e6f768e63c
Pudasaini, Siddhartha <siddharthapudasaini@my.unt.edu>
2024-10-20 12:19:03 -0500

    Added dot env file

--- frontend/src/Firebase/firebase.js
+++ frontend/src/Firebase/firebase.js
@@ -3,15 +3,14 @@ import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getAuth } from "firebase/auth";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyCM9wlmy3oVh5tM5Zpn-0W0MuwGIfeQdw0",
-    authDomain: "shop-simplify.firebaseapp.com",
-    projectId: "shop-simplify",
-    storageBucket: "shop-simplify.appspot.com",
-    messagingSenderId: "997557810347",
-    appId: "1:997557810347:web:5ab2cd08d01da2f89473ee",
-    measurementId: "G-VTV1821RQN"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
