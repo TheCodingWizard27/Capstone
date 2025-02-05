477d5e0b14b7f1cfef2830eae3473a18a100cd6a
Pudasaini, Siddhartha <siddharthapudasaini@my.unt.edu>
2025-02-05 10:25:23 -0600

    Misc

---
+++ backend/routes/message.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const router = express.Router();
+const verifyAuthToken = require("../middlewares/authMiddleware");
+const messageController = require("../controllers/messageController");
+
+//Post message
+router.post("/sendMessage", verifyAuthToken, messageController.addMessage);
+
+// Fetch conversation threads
+router.get("/getAllThreads", messageController.getAllThreads);
+
+//Fetch conversation by threads
+router.get("/getThreadMessage", messageController.getMessageByThread);
+
+module.exports = router;
