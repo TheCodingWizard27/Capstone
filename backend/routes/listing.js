0d7ed385df105b000c3403539699083ba972cb76
Panthi2001 <kushalpanthi13@gmail.com>
2025-03-14 21:02:39 -0500

    adding routes for delete end point

--- backend/routes/listing.js
+++ backend/routes/listing.js
@@ -25,11 +25,18 @@ router.get("/listings/:id", listingController.getSingleListing);
 router.get("/search", listingController.searchListings);
 router.get("/listings/category/:category", getListingsByCategory);
 
+//fetchiing by my listings
+router.get("/my-listings/:userId", listingController.getMyListings);
+
+
+
 router.put(
   "/updateListing/:id",
   verifyAuthToken,
   upload,
   listingController.updateListing
 );
+router.delete("/listings/:id", verifyAuthToken, listingController.deleteListing);
 
+router.put("/listings/:id/status", verifyAuthToken, listingController.updateListingStatus);
 module.exports = router;
