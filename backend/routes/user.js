38bad79fc41cbc3c3e9599efdbde7f547f8bcd77
Panthi2001 <kushalpanthi13@gmail.com>
2025-04-01 20:57:22 -0500

    routes added ..2 routes added put and get for user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,8 +1,14 @@
 const express = require("express");
+const multer = require("multer");
 const router = express.Router();
 const verifyAuthToken = require("../middlewares/authMiddleware");
 const userController = require("../controllers/userController");
 
+const upload = multer({ storage: multer.memoryStorage() }); // Store files in memory
+
 router.post("/addUser", verifyAuthToken, userController.addUser);
+router.put("/updateUserInfo", verifyAuthToken, upload.single("photo"), userController.updateUserInfo);
+router.put("/changePassword", verifyAuthToken, userController.changePassword);
+router.get("/userInfo", verifyAuthToken, userController.getUserInfo);
 
 module.exports = router;
